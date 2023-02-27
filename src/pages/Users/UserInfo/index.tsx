9aa8e28dff15f0e1c100c7d764499f31072e418e
d70900 <oluseun.oladiipo@ubagroup.com>
2023-02-27 03:22:59 +0100

    routine: corrected file import error on user info

--- src/pages/Users/UserInfo/index.tsx
+++ src/pages/Users/UserInfo/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import dayjs from 'dayjs';
-import './userInfo.scss';
+import './userinfo.scss';
 import { Grid, Rating } from '@mui/material';
 
 type UserProps = {
